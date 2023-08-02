382a85803130f4e72c91a48af6df41f7ca0488cf
duanxinjian <wydxjcrf157@126.com>
2023-08-02 09:52:21 +0800

    增加搜索框和banner图片

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,20 @@
 <template>
   <div class="wrapper">
-      <div class="position">
-        <span class="iconfont position__icon">&#xe619;</span>
-        北京理工大学国防科技园2号楼10层
-        <span class="iconfont position_notice">&#xe60b;</span>
-      </div>
+    <div class="position">
+      <span class="iconfont position__icon">&#xe619;</span>
+      北京理工大学国防科技园2号楼10层
+      <span class="iconfont position_notice">&#xe60b;</span>
+    </div>
+    <div class="search">
+      <span class="iconfont">&#xe62d;</span>
+      <span class="search__text">山姆会员商店优惠商品</span>
+    </div>
+    <div class="banner">
+        <img
+          class="banner__img"
+          src="http://www.dell-lee.com/imgs/vue3/banner.jpg"
+        />
+    </div>
   </div>
   <div class="docker">
     <div class="docker__item docker__item--active">
@@ -14,7 +24,7 @@
     <div class="docker__item">
       <div class="iconfont">&#xe7e5;</div>
       <div class="docker__title">购物车</div>
-      </div>
+    </div>
     <div class="docker__item">
       <div class="iconfont">&#xe61e;</div>
       <div class="docker__title">订单</div>
@@ -27,61 +37,84 @@
 </template>
 
 <style lang="scss">
-@import './style/viriables.scss';
-@import './style/mixins.scss';
+@import "./style/viriables.scss";
+@import "./style/mixins.scss";
 .docker {
   display: flex;
   box-sizing: border-box;
   position: absolute;
-  padding: 0 .18rem;
+  padding: 0 0.18rem;
   left: 0;
   bottom: 0;
   width: 100%;
-  height: .49rem;
-  border-top: .01rem solid #F1F1F1;
+  height: 0.49rem;
+  border-top: 0.01rem solid #f1f1f1;
   color: $content-fontcolor;
   &__item {
     flex: 1;
     text-align: center;
     .iconfont {
-      margin: .07rem 0 .02rem 0;
-      font-size: .18rem;
+      margin: 0.07rem 0 0.02rem 0;
+      font-size: 0.18rem;
     }
     &--active {
-      color: #1FA4FC;
+      color: #1fa4fc;
     }
   }
   &__title {
-    font-size: .2rem;
-    transform: scale(.5, .5);
+    font-size: 0.2rem;
+    transform: scale(0.5, 0.5);
     transform-origin: center top;
   }
 }
-.wrapper{
+.wrapper {
   position: absolute;
   left: 0;
   top: 0;
   right: 0;
-  bottom: .5rem;
-  padding: 0 .18rem;
+  bottom: 0.5rem;
+  padding: 0 0.18rem;
 }
-.position{
+.position {
   position: relative;
-  padding: .16rem .24rem .16rem 0;
-  line-height: .22rem;
-  font-size: .16rem;
+  padding: 0.16rem 0.24rem 0.16rem 0;
+  line-height: 0.22rem;
+  font-size: 0.16rem;
   @include ellipsis;
   color: $content-fontcolor;
-  .position__icon{
+  .position__icon {
     position: relative;
-    top: .01rem;
-    font-size: .2rem;
+    top: 0.01rem;
+    font-size: 0.2rem;
   }
-  .position_notice{
+  .position_notice {
     position: absolute;
-    top: .17rem;
+    top: 0.17rem;
     right: 0;
-    font-size: .2rem;
+    font-size: 0.2rem;
+  }
+}
+.search {
+  line-height: 0.32rem;
+  background: #f5f5f5;
+  color: #b7b7b7;
+  border-radius: 0.16rem;
+  .iconfont {
+    display: inline-block;
+    font-size: 0.2rem;
+    padding: 0 0.08rem 0 0.16rem;
+  }
+  &__text {
+    display: inline-block;
+    font-size: 0.14rem;
+  }
+}
+.banner{
+  height: 0;
+  overflow: hidden;
+  padding-bottom: 25.4%;
+  &__img{
+    width: 100%;
   }
 }
 </style>
